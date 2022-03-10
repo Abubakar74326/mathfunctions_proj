f314e2ad144c1911d13225af7ce037712b557321
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-10 05:19:55 -0500

    api

---
+++ apiflask.py
@@ -0,0 +1,24 @@
+# from typing import Optional
+# from fastapi import FastAPI
+
+from flask import Flask
+import allfunctions
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.route("/function/<int:num>/") #math/
+def read_item(num: int): #math_func
+    object=allfunctions.Mathematics(int(num))
+    results={"number": int(num),
+             "factorial": object.factorial(),
+             "square": object.square(),
+             "cube": object.cube(),
+             }
+
+    return str(results)
