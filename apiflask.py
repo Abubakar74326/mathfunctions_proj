432562608589d75c1842beb74adb058861c4b23e
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-10 05:29:09 -0500

    correct coded intent

--- apiflask.py
+++ apiflask.py
@@ -12,13 +12,13 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.route("/function/<int:num>/") #math/
-def read_item(num: int): #math_func
-    object=allfunctions.Mathematics(int(num))
-    results={"number": int(num),
-             "factorial": object.factorial(),
-             "square": object.square(),
-             "cube": object.cube(),
-             }
-
+@app.route("/function/<int:num>/")  # math/
+def read_item(num: int):  # math_func
+    object = allfunctions.Mathematics(int(num))
+    results = {
+        "number": int(num),
+        "factorial": object.factorial(),
+        "square": object.square(),
+        "cube": object.cube(),
+    }
     return str(results)
